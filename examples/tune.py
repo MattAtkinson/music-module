40fea0e8ec2345ff536ad4f84c49be88f0f6c4ee
Matt Atkinson <matt.atkinson@imgtec.com>
2016-08-22 15:15:08 +0100

    Add an example

---
+++ examples/tune.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import mm
+
+####################################
+
+for x in range (0, 2):
+	mm.set_c8()
+	mm.quarter_play()
+
+for x in range (0, 2):
+	mm.set_g8()
+	mm.quarter_play()
+
+for x in range (0, 2):
+	mm.set_a8()
+	mm.quarter_play()
+
+mm.set_c8()
+mm.half_play()
+
+####################################
